875842371299b952cc519e42de5f7c3590c79901
byOscarPINE <emanuelsant768@gmail.com>
2024-09-12 07:31:44 -0600

    Up1

---
+++ src/app/app.routes.ts
@@ -0,0 +1,19 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { InventoryComponent } from './pages/inventory/inventory.component';
+
+export const routes: Routes = [
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'about',
+        component: AboutComponent,
+    },
+    {
+        path: 'inventory',
+        component: InventoryComponent,
+    }
+];
