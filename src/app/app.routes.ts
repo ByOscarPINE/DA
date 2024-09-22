8c2b1bd5cf621ea956179797e9c3ec7243326780
byOscarPINE <emanuelsant768@gmail.com>
2024-09-21 18:08:53 -0600

    Create services

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { InventoryComponent } from './pages/inventory/inventory.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
 
 export const routes: Routes = [
     {
@@ -15,6 +16,10 @@ export const routes: Routes = [
     {
         path: 'inventory',
         component: InventoryComponent,
+    },
+    {
+        path: 'product/:id',
+        component: ProductDetailComponent,
     },
     {
         path: '**',
--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -9,4 +9,31 @@ import { Component } from '@angular/core';
 })
 export class AboutComponent {
 
+  ver = false;
+  
+  user = {
+    name: 'Oscar Emanuel Santos Lopez', 
+    description: 'Soy estudiante de la carrera de Ingenieria en Tecnologias de la informacion, me gustan los videjuegos y la programacion',
+  }
+
+  videojuegos: string[] = [
+    'Halo',
+    'Tomb Rider',
+    'Forza Horizon',
+    'DOOM',
+    'Gears of War',
+    'Fornite',
+    'Call of Duty',
+    'Battlefield',
+    'Minecraft',
+    'Hellblade',
+    'A Plague Tale',
+    'Left 4 Dead',
+  ]
+
+  ocultarInfo(){
+    console.log('Ocultar info');
+    this.ver = !this.ver;
+  }
+
 }
